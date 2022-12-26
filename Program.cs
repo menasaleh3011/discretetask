d19a69fdd9c7c3edd138eb0c1b890c8b98fd70d4
menasaleh3011 <121459130+menasaleh3011@users.noreply.github.com>
2022-12-26 22:09:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int start = int.Parse(Console.ReadLine());
+int end = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = start; i <= end; i++)
+{
+    for (int x = 1; x <= i; x++)
+    {
+        if (i % x == 0)
+            sum++;
+    }
+    if (sum == 2)
+    {
+        Console.WriteLine(i);
+        sum = 0;
+    }
+    else
+    {
+        sum = 0;
+        continue;
+    }
+}
